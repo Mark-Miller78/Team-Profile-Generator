eeca8cfbf23c1197de12fec809c13b327bb3fc8f
Mark Miller <markmiller7858@gmail.com>
2022-01-16 13:44:53 -0500

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const pageTemplate = require('./src/page-template');
 const{writeFile} = require('./utils/generate-HTML');
-const mock = require('./src/mock-data');
+
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
